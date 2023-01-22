a9117a0ebe9869528acb9a3855544789f1f28a0c
hamzaallaa <hamzaallaa00@gmail.com>
2023-01-22 21:57:08 +0100

    push

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,8 @@
 exports.test=async(req,res)=>{
+    console.log("test===================>")
+    res.status(200).send("ok")
+}
+exports.getTest=async(req,res)=>{
+    console.log("test===================>")
     res.status(200).send("ok")
 }
--- router/route.js
+++ router/route.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const indexControlles = require("../controller/index.js");
 const indexMiddlewares = require("../middlewares");
 router.post("/test/",indexMiddlewares.verify,indexControlles.test);
+router.get("/getTest/",indexMiddlewares.verify,indexControlles.getTest);
 module.exports = router;
